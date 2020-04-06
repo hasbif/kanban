eee93780aba9237406632ba57c58278b74995505
hasbif <hasbisenpai@gmail.com>
2020-04-06 20:34:49 +0700

    change app remove get/

--- server/app.js
+++ server/app.js
@@ -6,6 +6,6 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req, res) => res.status(200).json({ msg: 'Hello World' }))
+app.get('/test', (req, res) => res.status(200).json({ msg: 'Hello World' }))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
